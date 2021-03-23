80996738750a2d1bffbe2c78ed03f7dd028e93ff
Bek <kruzus@live.com>
2021-03-23 04:03:08 -0400

    added few things

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,13 @@
+import "./style.scss"
+
+export const Home = () => {
+    return(
+        <>
+
+            <h1 className="h1_styler">
+                Welcome back, <span className="spanner">User</span>.
+            </h1>
+
+        </>
+    )
+}
---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,23 @@
+import "./style.scss"
+
+export const Navigation = () => {
+    return(
+        <>
+
+            <header>
+                <nav className="navbar">
+                    <h3 className="logo">KruzusTV</h3>
+                    <ul className="nav_links">
+                        <li>Link A</li>
+                        <li>Link B</li>
+                        <li>Link C</li>
+                        <li>Link D</li>
+                    </ul>
+
+                </nav>
+            </header>
+
+
+        </>
+    )
+}
