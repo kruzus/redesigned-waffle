e275197ae278be8ed8f1ef00b3d29b76fb8cd898
Bek <kruzus@live.com>
2021-03-23 07:13:46 -0400

    not sure

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,39 @@
 import "./style.scss"
+import {Button, Container} from "@material-ui/core";
+import React, {useEffect, useState} from "react";
+import TextField from "@material-ui/core/TextField";
+
+
+const Filter: RegExp = /^[a-zA-Z ]*$/
 
 export const Home = () => {
+    const [name, setName] = useState("")
+
+
+    const handleChange = (e) => {
+        const val = e.target.value;
+        if (Filter.test(val)) {
+            setName(e.target.value)
+        } else {
+            console.log("error")
+        }
+
+    }
+
+
+
     return(
         <>
-
+            <Container fixed>
             <h1 className="h1_styler">
-                Welcome back, <span className="spanner">User</span>.
+            Welcome back, <span className="spanner">{name ? name : "User"}</span>.
+
             </h1>
+
+                {/* eslint-disable-next-line @typescript-eslint/no-unused-expressions */}
+                <TextField   onChange={handleChange} color="primary" label="Enter your name" />
+            </Container>
+
 
         </>
     )
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,23 +1,23 @@
 import "./style.scss"
+import TextField from "@material-ui/core/TextField";
+import React, {useState} from "react";
+
 
 export const Navigation = () => {
     return(
         <>
+        <header>
+               <nav className="navbar">
+               <h3 className="logo">uwu</h3>
+               <ul className="nav_links">
+               <li>Home</li>
+               <li>Users</li>
+               <li>Dashboard</li>
+               <li>Logout</li>
+              </ul>
 
-            <header>
-                <nav className="navbar">
-                    <h3 className="logo">LOGO</h3>
-                    <ul className="nav_links">
-                        <li>Link A</li>
-                        <li>Link B</li>
-                        <li>Link C</li>
-                        <li>Link D</li>
-                    </ul>
-
-                </nav>
-            </header>
-
-
+        </nav>
+        </header>
         </>
     )
 }
